7643ce2b84cd7e2280882f391454d5c800b3888d
x3x31 <x3x31@hotmail.com>
2020-03-29 22:34:58 -0300

    teste

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const routes = require('./routes')
 
 const app = express();
 
+
 //app.use(cors({origin:'http://meuapp.com'})); //módulo de segurança que vai determinar quem vai poder acessar a aplicação em ambiente de PRODUÇÃO 
 app.use(cors()); //módulo de segurança que vai determinar quem vai poder acessar a aplicação em ambiente de DESENVIMENTO 
 app.use(express.json()); //Para entender que será recebido um JSON no corpa da requisição 
